9eac93419d21b1186b987b78e6a5ab6dddad830c
Hakan Bilgin <hbi@longscript.com>
2019-12-21 02:46:35 +0100

    first commit

---
+++ res/js/classes/storeManager.js
@@ -0,0 +1,24 @@
+
+export class LocalStorageManager {
+	constructor() {
+		this.bestScoreKey = "bestScore";
+		this.gameStateKey = "gameState";
+		this.storage = window.storage;
+	}
+	getBestScore() {
+		return this.storage.getItem(this.bestScoreKey) || 0;
+	}
+	setBestScore(score) {
+		this.storage.setItem(this.bestScoreKey, score);
+	}
+	getGameState() {
+		var stateJSON = this.storage.getItem(this.gameStateKey);
+		//return stateJSON ? JSON.parse(stateJSON) : null;
+	}
+	setGameState(gameState) {
+		//this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+	}
+	clearGameState() {
+		this.storage.removeItem(this.gameStateKey);
+	}
+}
---
+++ res/js/main.js
@@ -0,0 +1,30 @@
+
+import { GameManager } from "./classes/gameManager";
+import { HTMLActuator } from "./classes/htmlActuator";
+import { LocalStorageManager } from "./classes/storeManager";
+
+const g2048 = {
+	init() {
+		this.gameManager = new GameManager(4, HTMLActuator, LocalStorageManager);
+	},
+	dispatch(event) {
+		switch (event.type) {
+			case "keystroke":
+				switch (event.char) {
+					case "up":    this.gameManager.move(0); break;
+					case "down":  this.gameManager.move(2); break;
+					case "left":  this.gameManager.move(3); break;
+					case "right": this.gameManager.move(1); break;
+				}
+				break;
+			case "restart":
+				this.gameManager.restart();
+				break;
+			case "keep-playing":
+				this.gameManager.doKeepPlaying();
+				break;
+		}
+	}
+};
+
+window.exports = g2048;
